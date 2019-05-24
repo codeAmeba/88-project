ac227290bc12b811bb2704c4c86fb730d5fa6733
jeongsooyoung <code.ameba@gmail.com>
2019-05-24 15:38:57 +0900

    15:38 modified

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 
+const orderCheck = document.querySelector('.popup');
+orderCheck.addEventListener('click', function () {
+  alert('???');
+})
 
-function onclick() {
-  return console.log(mySandwich);
-}
+const mySandwich = document.querySelectorAll('input');
 
-let myBread = document.querySelector('.breadList');
+
+let myBread = document.getElementsByClassName('myBread');
+
 let myBreadSelect = '';
 for (let i = 0; i < myBread.length; i++) {
-  if (myBread[i].checked) myBreadSelect += (myBread[i].name);
-} console.log(myBreadSelect);
+  if (myBread[i].checked) myBreadSelect += myBread[i].name;
+}
+
+
+// const str = '';
+// str += adding comments;
+// `정수영바보${str}`
+
+
+
 
